ad5e776166e255ed33ec7f26e4e74e88851f661d
Eesa Zahed <eszhd1@gmail.com>
2023-01-12 21:02:18 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Fizzbuzz
+
+I'm trying to learn a new programming language (Rust)
+
+Install rust (macOS):
+
+```
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+export PATH="$HOME/.cargo/bin"
+
+rustc main.rs && ./main
+```
---
+++ main.rs
@@ -0,0 +1,20 @@
+
+fn main() {
+    fizzbuzz();
+}
+
+// run fizzbuzz up to 100
+
+fn fizzbuzz() {
+    for number in 1..101 {
+        if number % 15 == 0 {
+            println!("Fizzbuzz");
+        } else if number % 5 == 0 {
+            println!("Buzz");
+        } else if number % 3 == 0 {
+            println!("Fizz");
+        } else {
+            println!("{}", number);
+        }
+    }
+}
